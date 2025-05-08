2d3f07bf89e5cc25df5a64cc2c0562be0c199f65
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-05-08 22:26:45 +0900

    chore: 사용자 메인 화면 이미지 추가

---
+++ src/assets/main/index.js
@@ -0,0 +1,5 @@
+import BalanceImg from './img_balance.png'
+import BuildingImg from './img_building.png'
+import WorkspaceImg from './img_workspace.png'
+
+export { BalanceImg, BuildingImg, WorkspaceImg }
