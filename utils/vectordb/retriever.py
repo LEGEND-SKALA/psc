fc8428352347f1352eb574b371e28f5c429cfae5
Laon <sb723071@gmail.com>
2025-05-09 00:37:45 +0900

    fix : warning message 출력 사항 수정

--- utils/vectordb/retriever.py
+++ utils/vectordb/retriever.py
@@ -1,15 +1,15 @@
 from langchain_community.vectorstores import FAISS
 from config import VECTORDB_DIRS
-from utils.embedder import embed_text
+from utils.embedder import embedding_model
 
 def search(target: str, query: str, k: int = 5):
     vectordb_dir = VECTORDB_DIRS[target]
-    vectordb = FAISS.load_local(vectordb_dir, embed_text, allow_dangerous_deserialization=True)
+    vectordb = FAISS.load_local(vectordb_dir, embedding_model, allow_dangerous_deserialization=True)
 
     return vectordb.similarity_search(query, k=k)
 
 def search_mmr(target: str, query: str, k: int = 5, fetch_k: int = 20):
     vectordb_dir = VECTORDB_DIRS[target]
-    vectordb = FAISS.load_local(vectordb_dir, embed_text, allow_dangerous_deserialization=True)
+    vectordb = FAISS.load_local(vectordb_dir, embedding_model, allow_dangerous_deserialization=True)
 
     return vectordb.max_marginal_relevance_search(query, k=k, fetch_k=fetch_k)
