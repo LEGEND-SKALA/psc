2f928c056cbad451403d1857011d23969dc63cc5
dannysmson <dannysmson@naver.com>
2025-05-08 11:47:24 +0900

    init: 초기 프로젝트 구조 설정

---
+++ navi-be/src/main/java/navi/navi_be/NaviBeApplication.java
@@ -0,0 +1,13 @@
+package navi.navi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaviBeApplication.class, args);
+	}
+
+}
---
+++ navi-be/src/test/java/navi/navi_be/NaviBeApplicationTests.java
@@ -0,0 +1,13 @@
+package navi.navi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
