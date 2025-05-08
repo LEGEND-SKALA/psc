83b084fbca87feb488fc935b8fa05e87b8df4ffd
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-05-08 22:27:04 +0900

    design: 네비게이션 바 구현

---
+++ src/components/common/Nav.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+import { NaviLogo } from '../../assets/common'
+import { ImExit } from 'react-icons/im'
+
+const Nav = () => {
+  return (
+    <NavSection>
+      <HomeBtn>
+        <NaviLogo />
+      </HomeBtn>
+      <NavItems>
+        <UserName>스칼라 님</UserName>
+        <ExitBtn>
+          <ImExit size={30} color="#FF8B8B" />
+        </ExitBtn>
+      </NavItems>
+    </NavSection>
+  )
+}
+export default Nav
+
+const NavSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 1rem 3rem;
+  box-sizing: border-box;
+`
+const HomeBtn = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 7rem;
+  height: 7rem;
+`
+const NavItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`
+const UserName = styled.p`
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+const ExitBtn = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  background-color: #fff;
+  border-radius: 100%;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+  &:active {
+    background-color: #e0e0e0;
+  }
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`
