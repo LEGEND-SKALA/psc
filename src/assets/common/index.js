d9d3ea472edf5bf8a0fbefba4c50faaeee2f584e
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-05-09 16:14:21 +0900

    chore: 문서 관리 페이지 아이콘 추가

--- src/assets/common/index.js
+++ src/assets/common/index.js
@@ -1,2 +1,5 @@
+import FolderIcon from './ic_folder.png'
+
+export { FolderIcon }
 export { ReactComponent as SKLogo } from './ic_SK_logo.svg'
 export { ReactComponent as NaviLogo } from './ic_Navi_logo.svg'
