329434bf4945119c4957ff4732a44a6a0efe421f
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-05-10 17:25:18 +0900

    feat: 문서 목록 조회 및 삭제 서버 연동 테스트

--- src/components/admin/MainDocument.jsx
+++ src/components/admin/MainDocument.jsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { FileUpload } from '.'
 import { SlArrowUp, SlArrowDown } from 'react-icons/sl'
+import axios from 'axios'
 
 const MainDocument = () => {
+  const [documentList, setDocumentList] = useState([])
+  const [editList, setEditList] = useState([])
+
+  useEffect(() => {
+    fetchDocumentList()
+  }, [])
+
+  const fetchDocumentList = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/documents`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('NaviToken')}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.code === 0) {
+          setDocumentList(response.data.body)
+        } else {
+          alert('문서 목록을 가져오는 데 실패했습니다.')
+        }
+      })
+      .catch((error) => {
+        console.error('문서 목록 가져오기 오류:', error)
+        alert('문서 목록을 가져오는 중 오류가 발생했습니다.')
+      })
+  }
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`${process.env.REACT_APP_SERVER_URL}/documents/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('NaviToken')}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.code === 0) {
+          alert('문서 삭제 성공')
+          fetchDocumentList()
+        } else {
+          alert('문서 삭제 실패')
+        }
+      })
+      .catch((error) => {
+        console.error('문서 삭제 오류:', error)
+        alert('문서 삭제 중 오류가 발생했습니다.')
+      })
+  }
+
+  const handleTitleClick = (id) => {
+    if (window.confirm('정말로 삭제하시겠습니까?')) {
+      handleDelete(id)
+    }
+  }
+
   return (
     <>
       <TitleTop>
@@ -62,11 +120,12 @@ const MainDocument = () => {
                 {documentList.map((item, index) => (
                   <DocumentItem
                     key={index}
-                    fileName={item.fileName}
-                    fileType={item.fileType}
-                    securityLevel={item.securityLevel}
-                    registeredDB={item.registeredDB}
-                    uploadDate={item.uploadDate}
+                    id={item.id}
+                    fileName={item.title}
+                    securityLevel={item.security}
+                    registeredDB={item.category}
+                    uploadDate={item.createdAt}
+                    handleTitleClick={handleTitleClick}
                   />
                 ))}
               </ListItems>
@@ -96,11 +155,12 @@ const MainDocument = () => {
                 {documentList.map((item, index) => (
                   <DocumentItem
                     key={index}
-                    fileName={item.fileName}
-                    fileType={item.fileType}
-                    securityLevel={item.securityLevel}
-                    registeredDB={item.registeredDB}
-                    uploadDate={item.uploadDate}
+                    id={item.id}
+                    fileName={item.title}
+                    securityLevel={item.security}
+                    registeredDB={item.category}
+                    uploadDate={item.createdAt}
+                    handleTitleClick={handleTitleClick}
                   />
                 ))}
               </ListItems>
@@ -115,14 +175,24 @@ export default MainDocument
 
 const DocumentItem = ({
   fileName,
-  fileType,
   securityLevel,
   registeredDB,
   uploadDate,
+  id,
+  handleTitleClick,
 }) => {
+  let fileType = fileName.split('.').pop()
+  fileType = fileType == 'pdf' ? 'PDF' : 'TEXT'
+  const nameWithoutExtension = fileName.slice(0, fileName.lastIndexOf('.'))
+  securityLevel =
+    securityLevel === 'HIGH' ? '상' : securityLevel === 'MEDIUM' ? '중' : '하'
+  registeredDB = registeredDB === 'SPACE' ? '공간' : '법률'
+  uploadDate = uploadDate.split('T')[0]
+  uploadDate = uploadDate.replace(/-/g, '.')
+
   return (
     <DocumentItemWrapper>
-      <div>{fileName}</div>
+      <div onClick={() => handleTitleClick(id)}>{nameWithoutExtension}</div>
       <div>
         <Tag
           color={fileType === 'PDF' ? '#6D5C9D' : '#5C5C5C'}
@@ -379,55 +449,3 @@ const DownBtn = styled(EditBtn)`
     background-color: #3c3c3c;
   }
 `
-
-const documentList = [
-  {
-    fileName: '2025_신입사원_교육자료',
-    fileType: 'PDF',
-    securityLevel: '하',
-    registeredDB: '법률',
-    uploadDate: '2025.05.08 09:55',
-  },
-  {
-    fileName: '2025_규내자료_최종',
-    fileType: 'PDF',
-    securityLevel: '상',
-    registeredDB: '법률',
-    uploadDate: '2025.05.04 19:03',
-  },
-  {
-    fileName: '신입사원_교육자료_보충',
-    fileType: 'TEXT',
-    securityLevel: '중',
-    registeredDB: '공간',
-    uploadDate: '2025.04.26 14:20',
-  },
-  {
-    fileName: '2025_사옥정보',
-    fileType: 'PDF',
-    securityLevel: '상',
-    registeredDB: '공간',
-    uploadDate: '2025.04.26 10:01',
-  },
-  {
-    fileName: '2025_신입사원_교육자료',
-    fileType: 'PDF',
-    securityLevel: '하',
-    registeredDB: '법률',
-    uploadDate: '2025.05.08 09:55',
-  },
-  {
-    fileName: '2025_규내자료_최종',
-    fileType: 'PDF',
-    securityLevel: '상',
-    registeredDB: '법률',
-    uploadDate: '2025.05.04 19:03',
-  },
-  {
-    fileName: '신입사원_교육자료_보충',
-    fileType: 'TEXT',
-    securityLevel: '중',
-    registeredDB: '공간',
-    uploadDate: '2025.04.26 14:20',
-  },
-]
