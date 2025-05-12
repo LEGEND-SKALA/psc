8f420460ecb8f91398322c8d55c88f163127bf9a
dannysmson <dannysmson@naver.com>
2025-05-12 09:02:39 +0900

    fix : 파일 업로드 수정

--- navi-be/src/main/java/navi/navi_be/document/service/DocumentService.java
+++ navi-be/src/main/java/navi/navi_be/document/service/DocumentService.java
@@ -7,13 +7,8 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,13 +24,10 @@ import navi.navi_be.document.repository.DocumentRepository;
 public class DocumentService {
 
     private final DocumentRepository documentRepository;
-    private final WebClient webClient;  // ✅ WebClient 주입받음
 
     @Value("${document.upload.dir}")
     private String uploadDir;
 
-    @Value("${http://10.250.72.251:8000}") // 예: http://192.168.0.10:8000
-    private String fastapiServerUrl;
 
     // 문서 업로드 + FastAPI로 전달
     public void uploadDocument(DocumentUploadRequest request) {
@@ -58,34 +50,7 @@ public class DocumentService {
                 .filePath(dest.getAbsolutePath())
                 .status(DocumentStatus.ACTIVE)
                 .build();
-        documentRepository.save(document);
-
-        // ✅ FastAPI에 /add 요청
-        try {
-    byte[] bytes = file.getBytes();  // MultipartFile에서 byte 배열 추출
-
-    MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
-    bodyBuilder.part("file", new ByteArrayResource(bytes) {
-                @Override
-                public String getFilename() {
-                    return file.getOriginalFilename();  // 파일명 설정
-                }
-            })
-            .contentType(MediaType.APPLICATION_PDF);
-    bodyBuilder.part("category", request.getCategory());
-
-    webClient.post()
-            .uri(fastapiServerUrl + "/add")
-            .contentType(MediaType.MULTIPART_FORM_DATA)
-            .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
-            .retrieve()
-            .bodyToMono(String.class)
-            .doOnNext(response -> log.info("FastAPI 응답: {}", response))
-            .block();
-
-} catch (IOException e) {
-    throw new RuntimeException("FastAPI 호출 중 오류", e);
-}
+        documentRepository.save(document);       
     }
 
     public List<DocumentResponse> getActiveDocuments() {
